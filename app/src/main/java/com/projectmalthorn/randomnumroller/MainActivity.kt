219e94b85d4c9b88c248eec94453ebd8c2158095
Marlon Aleman <mjaleman1997@gmail.com>
2021-06-27 20:03:37 -0700

    Add walking skeleton
    Fix: #1

---
+++ app/src/main/java/com/projectmalthorn/randomnumroller/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.projectmalthorn.randomnumroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+private const val LIMIT = 20
+
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollNumber()}
+
+    }
+
+    private fun rollNumber() {
+        val randomNum =  (1..LIMIT).random()
+
+        // Links to the text view
+        val textView: TextView = findViewById(R.id.number_output)
+
+        //Sets the TextView to the random integer
+        textView.text = randomNum.toString()
+
+        // _result.value = randomInt.toString()
+    }
+
+}
+
